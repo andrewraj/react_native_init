0aae0ef032b752d7f5ea7dfc91c0e1b3561bad64
Andrew Raj <me@andrewraj.com>
2018-08-16 16:10:08 +0800

    Integrated redux and saga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import { Provider } from "react-redux";
+import configureStore from './redux/config';
 import {StackNavigator} from './navigator'
 export default class App extends Component {
   render() {
     return (
+      <Provider store={configureStore()}>
       <StackNavigator/>
+      </Provider>
     );
   }
 }
---
+++ src/redux/reducer/data.js
@@ -0,0 +1,10 @@
+const initialState = {
+  title: 'Welcome',
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+    return state
+  }
+}
